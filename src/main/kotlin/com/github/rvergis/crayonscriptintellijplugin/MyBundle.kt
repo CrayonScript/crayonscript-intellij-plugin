73e9f69cd7c0058ce53111c186a94255cbbc6d19
GitHub Action <action@github.com>
2021-04-16 16:01:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/rvergis/crayonscriptintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/rvergis/crayonscriptintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rvergis.crayonscriptintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/rvergis/crayonscriptintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rvergis.crayonscriptintellijplugin.services
+
+import com.github.rvergis.crayonscriptintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
