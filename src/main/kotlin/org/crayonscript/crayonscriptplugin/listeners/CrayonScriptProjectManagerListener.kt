9a9f42de49289f4aa1b5d8343466e976951d0a07
Ron Vergis <ron.vergis@gmail.com>
2021-04-19 09:13:42 -0500

    Update Project manager listener class name

--- src/main/kotlin/org/crayonscript/crayonscriptplugin/listeners/CrayonScriptProjectManagerListener.kt
+++ src/main/kotlin/org/crayonscript/crayonscriptplugin/listeners/CrayonScriptProjectManagerListener.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import org.crayonscript.crayonscriptplugin.services.CrayonScriptProjectService
 
-internal class MyProjectManagerListener : ProjectManagerListener {
+internal class CrayonScriptProjectManagerListener : ProjectManagerListener {
 
     override fun projectOpened(project: Project) {
         project.service<CrayonScriptProjectService>()
