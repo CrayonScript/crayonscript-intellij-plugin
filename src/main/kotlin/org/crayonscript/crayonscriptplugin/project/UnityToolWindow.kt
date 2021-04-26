5dcf012df57382eac83ba27246173d81da2812ff
Ron Vergis <ron.vergis@gmail.com>
2021-04-26 16:57:42 -0500

    wip new project tool window

---
+++ src/main/kotlin/org/crayonscript/crayonscriptplugin/project/UnityToolWindow.kt
@@ -0,0 +1,15 @@
+package org.crayonscript.crayonscriptplugin.project
+
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class CrayonScriptUnityToolWindowFactory : ToolWindowFactory, DumbAware {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO("Not yet implemented")
+    }
+}
+
+class UnityToolWindow {
+}
